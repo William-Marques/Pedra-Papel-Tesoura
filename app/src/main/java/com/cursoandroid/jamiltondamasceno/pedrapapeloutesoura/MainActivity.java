6a2bc48bdf31767a19ce6cc4a13b787066c6a215
William Marques <matrix.consultoria@gmail.com>
2020-06-10 19:12:39 -0300

    Add Dark Theme

--- app/src/main/java/com/cursoandroid/jamiltondamasceno/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/cursoandroid/jamiltondamasceno/pedrapapeloutesoura/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.ThemeOverlay_AppCompat_Dark);
         setContentView(R.layout.activity_main);
     }
 
